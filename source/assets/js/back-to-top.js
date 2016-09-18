27936d7612bc890c93062bdb415ad87979033fd1
Rajiv Nair <rsnair2@me.com>
2016-09-18 14:55:11 -0700

    Revert "Adds back-to-top button;"
    This reverts commit 2e73770229563d0cf2fabd79d21fa1a0c16be8dc.

--- source/assets/js/back-to-top.js
+++
@@ -1,6 +0,0 @@
-// src: http://fiddle.jshell.net/Panman8201/MKZrm/10/light/
-if (($(window).height() + 100) < $(document).height()) {
-    $('#top-link-block').removeClass('hidden').affix({
-        offset: {top:100}
-    });
-}
